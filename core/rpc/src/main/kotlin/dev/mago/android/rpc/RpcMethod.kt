3423e37741bbbde0cc1f4c3d8cdf8deb85392948
eightkingdomsalliedforces-hash <eightkingdomsalliedforces@gmail.com>
2026-07-25 23:18:41 +0800

    feat: add module and console RPC methods

--- core/rpc/src/main/kotlin/dev/mago/android/rpc/RpcMethod.kt
+++ core/rpc/src/main/kotlin/dev/mago/android/rpc/RpcMethod.kt
@@ -16,5 +16,19 @@ value class RpcMethod(val value: String) {
         val AUTH_LOGOUT = RpcMethod("auth.logout")
         val HEALTH_CHECK = RpcMethod("health.check")
         val CORE_VERSION = RpcMethod("core.version")
+
+        val MODULE_EXPLOITS = RpcMethod("module.exploits")
+        val MODULE_AUXILIARY = RpcMethod("module.auxiliary")
+        val MODULE_POST = RpcMethod("module.post")
+        val MODULE_PAYLOADS = RpcMethod("module.payloads")
+        val MODULE_ENCODERS = RpcMethod("module.encoders")
+        val MODULE_NOPS = RpcMethod("module.nops")
+        val MODULE_EVASION = RpcMethod("module.evasion")
+        val MODULE_INFO = RpcMethod("module.info")
+
+        val CONSOLE_CREATE = RpcMethod("console.create")
+        val CONSOLE_READ = RpcMethod("console.read")
+        val CONSOLE_WRITE = RpcMethod("console.write")
+        val CONSOLE_DESTROY = RpcMethod("console.destroy")
     }
 }
