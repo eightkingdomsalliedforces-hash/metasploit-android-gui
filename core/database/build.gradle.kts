218abb8ebe6aa5bd714523f3835a9c77d0231d17
eightkingdomsalliedforces-hash <eightkingdomsalliedforces@gmail.com>
2026-07-26 11:41:00 +0800

    build: add module domain dependency to database cache

--- core/database/build.gradle.kts
+++ core/database/build.gradle.kts
@@ -8,11 +8,14 @@ dependencies {
     implementation(project(":core:common"))
     implementation(project(":core:model"))
     implementation(project(":domain:installation"))
+    implementation(project(":domain:metasploit"))
+    implementation(libs.coroutines.core)
     implementation(libs.room.runtime)
     implementation(libs.room.ktx)
     ksp(libs.room.compiler)
     testImplementation(libs.junit)
     testImplementation(libs.truth)
+    testImplementation(libs.coroutines.test)
 }
 
 room {
