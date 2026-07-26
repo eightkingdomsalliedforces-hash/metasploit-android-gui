348a2170d9e10d0cfe36226fd22f2ce7e3e163ce
eightkingdomsalliedforces-hash <eightkingdomsalliedforces@gmail.com>
2026-07-26 12:27:21 +0800

    test: verify integer session map keys

--- core/rpc/src/test/kotlin/dev/mago/android/rpc/MessagePackRpcCodecTest.kt
+++ core/rpc/src/test/kotlin/dev/mago/android/rpc/MessagePackRpcCodecTest.kt
@@ -3,6 +3,7 @@ package dev.mago.android.rpc
 import com.google.common.truth.Truth.assertThat
 import dev.mago.android.model.rpc.RpcValue
 import org.junit.Test
+import org.msgpack.core.MessagePack
 
 class MessagePackRpcCodecTest {
     private val codec = MessagePackRpcCodec()
@@ -26,5 +27,22 @@ class MessagePackRpcCodecTest {
         val decoded = codec.decode(bytes) as RpcValue.ArrayValue
         assertThat((decoded.value[0] as RpcValue.StringValue).value).isEqualTo("core.version")
         assertThat((decoded.value[1] as RpcValue.StringValue).value).isEqualTo("token-1")
+    }
+
+    @Test
+    fun `integer map keys are normalized to decimal strings`() {
+        val bytes = MessagePack.newDefaultBufferPacker().use { packer ->
+            packer.packMapHeader(2)
+            packer.packInt(7)
+            packer.packString("session-seven")
+            packer.packString("8")
+            packer.packString("session-eight")
+            packer.toByteArray()
+        }
+
+        val decoded = codec.decode(bytes) as RpcValue.MapValue
+
+        assertThat((decoded.value["7"] as RpcValue.StringValue).value).isEqualTo("session-seven")
+        assertThat((decoded.value["8"] as RpcValue.StringValue).value).isEqualTo("session-eight")
     }
 }
